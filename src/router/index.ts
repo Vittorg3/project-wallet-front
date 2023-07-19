4509bb214f19c71307a08d9d341be82dddc2fbc6
Enoc Vitor <enoc.froes@edupulses.com.br>
2023-07-19 14:39:38 -0300

    wip: redirecionamento rotas

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AuthView from '../views/AuthView.vue'
 
+let isAuthenticated = true;
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -21,13 +23,27 @@ const router = createRouter({
     },
     {
       path: '/sign-in',
-      name: 'about',
+      name: 'sign-in',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AuthView.vue')
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (!isAuthenticated) {
+    if (to.path !== '/sign-in') {
+      next('/sign-in')
+    } else {
+      next()
+    }
+  } else if(isAuthenticated && to.path === '/sign-in') {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
