36cfba56b535a76a895173ef27627f3bef8c4acd
Enoc Vitor <enoc.froes@edupulses.com.br>
2023-07-19 15:19:32 -0300

    wip: header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Header from '@/components/header/Header.vue'
 </script>
 
 <template>
-  <RouterView />
+  <Header />
+  <RouterView class="router-view"/>
 </template>
 
 <style scoped>
+  .router-view {
+    height: calc(100% - 100px);
+  }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <main>
   </main>
 </template>
+
+<script setup lang="ts">
+</script>
 
 <style scoped>
   main {
