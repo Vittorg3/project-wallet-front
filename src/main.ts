af3029acb8173880a957b14440500b71f2441f98
Enoc Vitor <enoc.froes@edupulses.com.br>
2023-08-15 15:22:54 -0300

    wip: modal

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import Modal from '@/components/modal/Modal.vue'
+
 const app = createApp(App)
+
+// registrando componentes globalmente
+app.component('modal', Modal)
 
 app.use(createPinia())
 app.use(router)
